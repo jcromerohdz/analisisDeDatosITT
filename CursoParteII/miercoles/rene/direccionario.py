5d13fee40ff62ee5499cb7a99e358b3210f92304
René Solis <rsolis@tectijuana.edu.mx>
2017-08-09 12:54:51 -0700

    diccionario

---
+++ CursoParteII/miercoles/rene/direccionario.py
@@ -0,0 +1,18 @@
+user = {
+	"Angelica": 
+	{"Moderatto":3.5, "Caifanes":2.0, "Heroes del silencio":4.5,"Soda Stereo":5.0, "Tierra santa": 1.5, "Rata Banca":2.5, "Mago de Oz":2.0}, 
+	"Bill":
+	{"Moderatto":2.0, "Caifanes":3.5, "Heroes del silencio":4.5, "La Ley":4.0, "Soda Stereo":2.0, "Tierra santa": 3.5, "Rata Banca":2.0, "Mago de Oz":3.0}, 
+	"Chan":
+	{"Moderatto":5.0, "Caifanes":1.0, "Heroes del silencio":4.5, "La Ley":4.5, "Soda Stereo":3.0, "Tierra santa": 4.5, "Rata Banca":4.0, "Mago de Oz":1.0}, 
+	"Dan":
+	{"Moderatto":3.0, "Caifanes":4.0, "La Ley":4.5, "Soda Stereo":3.0, "Tierra santa": 4.5, "Rata Banca":4.5, "Mago de Oz":2.0}, 
+	"Hailey":
+	{Caifanes":4.0, "La Ley":1.0,  "Tierra santa": 4.0, "Rata Banca":4.0, "Mago de Oz":1.0}, 
+	"Jordyn":
+	{Caifanes":4.5, "La Ley":4.0,  "Heroes del silencio":5.0, "Tierra santa": 4.5, "Rata Banca":4.0, "Mago de Oz":4.0}, 
+	"Sam": 
+	{"Moderatto":5.0, "Caifanes":2.0, "Heroes del silencio":3.0,"Soda Stereo":5.0, "Tierra santa": 4.0, "Rata Banca":5.0},
+	"Veronica": 
+	{"Moderatto":3.0,  "Heroes del silencio":5.0, "Soda Stereo":4.0, "Tierra santa": 2.5, "Rata Banca":3.0},
+}
